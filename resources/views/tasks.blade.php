7234d7fd4e37a41a5626b1a47faf21f7d920ee38
thibault <thibault.sauron@insa-rouen.fr>
2018-06-06 20:11:34 +0200

    trhn

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -49,7 +49,7 @@
             @if (count($tasks) > 0)
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        Colocations
+                        Mes Colocataires
                     </div>
 
                     <div class="panel-body">
