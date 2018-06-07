6e9845e8b0e4618b46884339d556e90063a662d3
thibault <thibault.sauron@insa-rouen.fr>
2018-06-07 14:18:17 +0200

    zedf

--- resources/views/subs/Agen/agen.blade.php
+++ resources/views/subs/Agen/agen.blade.php
@@ -4,11 +4,11 @@
 <!-- demo stylesheet -->
 <link type="text/css" rel="stylesheet" href="media/layout.css">
 
-<link type="text/css" rel="stylesheet" href="themes/calendar_g.css">
-<link type="text/css" rel="stylesheet" href="themes/calendar_green.css">
-<link type="text/css" rel="stylesheet" href="themes/calendar_traditional.css">
-<link type="text/css" rel="stylesheet" href="themes/calendar_transparent.css">
-<link type="text/css" rel="stylesheet" href="themes/calendar_white.css">
+<link type="text/css" rel="stylesheet" href="calendar_g.css">
+<link type="text/css" rel="stylesheet" href="calendar_green.css">
+<link type="text/css" rel="stylesheet" href="calendar_traditional.css">
+<link type="text/css" rel="stylesheet" href="calendar_transparent.css">
+<link type="text/css" rel="stylesheet" href="calendar_white.css">
 
 <!-- helper libraries -->
 <script src="js/jquery-1.9.1.min.js" type="text/javascript"></script>
